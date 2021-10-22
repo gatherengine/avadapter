5954b355f729555429fbed5533dc9b6a3176ac4d
Duane Johnson <duane.johnson@gmail.com>
2021-10-22 11:22:43 -0600

    Restore video-mirror working state

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,17 @@
 <script lang="ts">
   import { VideoMirror } from "video-mirror";
-  import { RoomClient } from "./RoomClient";
+  // import { RoomClient } from "./RoomClient";
 
-  let room = new RoomClient("wss://media2.relm.us:4443", { room: "test" });
+  // let room = new RoomClient("wss://media2.relm.us:4443", { room: "test" });
 
   function join() {
-    room.join();
+    // room.join();
   }
 </script>
 
 <main>
-  <VideoMirror on:done={join} />
+  <VideoMirror />
+  <!-- <VideoMirror on:done={join} /> -->
 </main>
 
 <style>
